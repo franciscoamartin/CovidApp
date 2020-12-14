e0248517604614b17028f1f786a43dfbb93c3ac9
franciscoamartin <franciscoaugustomartin@gmail.com>
2020-12-14 00:04:20 -0300

    change title name app

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
     <div className='app'>
       <div className='app__left'>
         <div className='app__header'>
-          <h1>MAPA DO CORONA VÍRUS</h1>
+          <h1>COVID-19 TRACKER</h1>
           <FormControl className='app__dropdown'>
             <Select
               variant='outlined'
